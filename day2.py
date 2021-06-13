3572fbf8b7dae5cdfac5882571f954276200d294
Premsaivudya <85791573+Premsaivudya@users.noreply.github.com>
2021-06-13 23:00:21 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,44 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("30 days 30 hour challenge")
+30 days 30 hour challenge
+>>> Hours = "thirty"
+>>> print(Hours)
+thirty
+>>> Days = "Thirty days"
+>>> print(Days[0])
+T
+>>> print(Days[1])
+h
+>>> Challenge = "I will win"
+>>> print(Challenge[7:10])
+win
+>>> print(len(Challenge))
+10
+>>> print(Challenge.lower())
+i will win
+>>> a = "30 Days"
+>>> b = "30 hours"
+>>> c = a + b
+>>> print(c)
+30 Days30 hours
+>>> c = a + " " + b
+>>> print(c)
+30 Days 30 hours
+>>> text = "Thirty days and Thirty hours"
+>>> x = text.casefold()
+>>> print(x)
+thirty days and thirty hours
+>>> x = text.capitalize()
+>>> print(x)
+Thirty days and thirty hours
+>>> x = text.find('a')
+>>> print(x)
+8
+>>> x = text.isalpha()
+>>> print(x)
+False
+>>> x = text.isalnum()
+>>> print(x)
+False
+>>> 
