a36691facec80f97d81e234f79f328c5ed63eb78
Premsaivudya <85791573+Premsaivudya@users.noreply.github.com>
2021-07-14 08:59:49 +0530

    Add files via upload

---
+++ day26.py
@@ -0,0 +1,11 @@
+#Day 26
+#Task 1
+
+list1= ['uday','vamsi','krishna','pavan','alan']
+print(list(enumerate(list1)))
+
+print(enumerate(list1))
+
+#Task 2
+tuple1=('uday','vamsi','krishna','pavan','alan')
+print(tuple(enumerate(tuple1)))
